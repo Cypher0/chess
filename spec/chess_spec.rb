4e61e082da5f1695f981833e887d8f0b5f2b5f1a
Cypher <raiko4238@gmail.com>
2017-05-04 23:15:13 +0300

    add switch_plrs specs

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -27,4 +27,18 @@ describe 'Chess' do
       expect(game.board[11].coords).to eql([3,1])
     end
   end
+
+  describe '#switch_plrs' do
+
+    before { game.switch_plrs }
+
+    it 'switches to player2' do
+      expect(game.act_plr).to eql(game.plr2)
+    end
+
+    it 'switches back to player1' do
+      game.switch_plrs
+      expect(game.act_plr).to eql(game.plr1)
+    end
+  end
 end
