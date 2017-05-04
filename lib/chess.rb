84c65315b407ccfac90a24d6a9e71e47cbf8f34b
Cypher <raiko4238@gmail.com>
2017-05-04 23:07:16 +0300

    add main game class

---
+++ lib/chess.rb
@@ -0,0 +1,26 @@
+require_relative 'player'
+require_relative 'square'
+require_relative 'king'
+
+class Chess
+  attr_reader :plr1, :plr2
+  attr_accessor :act_plr, :board
+
+  def initialize(name_1, name_2)
+    @plr1 = Player.new(name_1, :white, WKing.new([4,0]))
+    @plr2 = Player.new(name_2, :black, BKing.new([4,7]))
+    @act_plr = @plr1
+    @rem_pieces = []
+    @taken_pieces = []
+    gen_board
+  end
+
+  def gen_board
+    @board = []
+    (0..7).each do |col|
+      (0..7).each do |row|
+        @board << Square.new([row,col])
+      end
+    end
+  end
+end
