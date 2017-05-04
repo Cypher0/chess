c66945ce82d7b35ded94ed06e704bf5fbc9911c1
Cypher <raiko4238@gmail.com>
2017-05-04 23:29:10 +0300

    add setup_pieces specs

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -41,4 +41,44 @@ describe 'Chess' do
       expect(game.act_plr).to eql(game.plr1)
     end
   end
+
+  describe '#setup_pieces' do
+
+    before do
+      game.gen_board
+      game.setup_pieces
+    end
+
+    it 'adds kings to correct positions' do
+      expect(game.board[4].piece).to be_a WKing
+      expect(game.board[-4].piece).to be_a BKing
+    end
+
+    it 'adds queens to correct positions' do
+      expect(game.board[3].piece).to be_a WQueen
+      expect(game.board[-5].piece).to be_a BQueen
+    end
+
+    it 'adds rooks to correct positions' do
+      expect(game.board[0].piece).to be_a WRook
+      expect(game.board[-1].piece).to be_a BRook
+    end
+
+    it 'adds knights to correct positions' do
+      expect(game.board[1].piece).to be_a WKnight
+      expect(game.board[-2].piece).to be_a BKnight
+    end
+
+    it 'adds bishops to correct positions' do
+      expect(game.board[2].piece).to be_a WBishop
+      expect(game.board[-3].piece).to be_a BBishop
+    end
+
+    it 'adds pawns to correct positions' do
+      expect(game.board[10].piece).to be_a WPawn
+      expect(game.board[15].piece).to be_a WPawn
+      expect(game.board[-9].piece).to be_a BPawn
+      expect(game.board[-15].piece).to be_a BPawn
+    end
+  end
 end
