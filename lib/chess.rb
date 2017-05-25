fafcc364cf80c56d5588dd9b5c98e5565d74e218
Cypher <raiko4238@gmail.com>
2017-05-25 22:38:01 +0300

    add check? method

--- lib/chess.rb
+++ lib/chess.rb
@@ -7,8 +7,8 @@ class Chess
 
   def initialize(name_1, name_2)
     @board = Board.new
-    @plr1 = Player.new(name_1, :white, @board.squares[4].piece)
-    @plr2 = Player.new(name_2, :black, @board.squares[-4].piece)
+    @plr1 = Player.new(name_1, :white)
+    @plr2 = Player.new(name_2, :black)
     @act_plr = @plr1
   end
 
@@ -26,5 +26,10 @@ class Chess
     @board.path_clear?(start, dest) &&
     (b.piece.nil? || @act_plr.color != b.piece.color) &&
     a.piece.poss_moves.any? { |mv| mv.map.with_index { |v, i| v + start[i] } == dest }
+  end
+
+  def check?(plr)
+    target = @board.rem_pieces.find { |pc| pc.class < King && pc.color == plr.color }
+    @board.rem_pieces.any? { |pc| pc.color != target.color && legal_move?(pc.pos, target.pos) }
   end
 end
