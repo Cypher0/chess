928646233b4cf7cdea423259e8b6fcdde7052673
Cypher <raiko4238@gmail.com>
2017-06-14 22:12:48 +0300

    add stalemate? functionality

--- lib/chess.rb
+++ lib/chess.rb
@@ -20,16 +20,38 @@ class Chess
                end
   end
 
-  def legal_move?(start, dest)
-    a = @board.squares.find { |sq| sq.coords == start }
-    b = @board.squares.find { |sq| sq.coords == dest }
-    @board.path_clear?(start, dest) &&
-    (b.piece.nil? || @act_plr.color != b.piece.color) &&
-    a.piece.poss_moves.any? { |mv| mv.map.with_index { |v, i| v + start[i] } == dest }
+  def legal_move?(start, dest, board = @board)
+    return false unless (start + dest).all? { |i| i.between?(0,7)}
+    a = board.squares.find { |sq| sq.coords == start }
+    b = board.squares.find { |sq| sq.coords == dest }
+    !a.piece.nil? &&
+    board.path_clear?(start, dest) &&
+    (b.piece.nil? || a.piece.color != b.piece.color) &&
+    a.piece.poss_moves.any? { |mv| [a.piece.pos[0] + mv[0], a.piece.pos[1] + mv[1]] == dest }
   end
 
-  def check?(plr)
-    target = @board.rem_pieces.find { |pc| pc.class < King && pc.color == plr.color }
-    @board.rem_pieces.any? { |pc| pc.color != target.color && legal_move?(pc.pos, target.pos) }
+  def check?(plr, board = @board)
+    king = board.rem_pieces.find { |pc| pc.class < King && pc.color == plr.color }
+    board.rem_pieces.any? { |pc| pc.color != king.color && legal_move?(pc.pos, king.pos) }
+  end
+
+  def stalemate?(plr, board = @board.dup)
+    temp_piece = []
+    @board.squares.each do |sq|
+      start = sq.coords
+      next if sq.piece.nil? || sq.piece.color != plr.color
+      sq.piece.poss_moves.each do |mv|
+        target = [start[0] + mv[0], start[1] + mv[1]]
+        if legal_move?(start, target)
+          temp_piece << board.squares.find { |sq| sq.coords == target }.piece unless board.squares.find { |sq| sq.coords == target }.piece.nil?
+          board.squares.find { |sq| sq.coords == target }.piece = nil
+          board.move(start, target)
+          return false unless check?(plr, board)
+          board.move(target, start)
+          board.squares.find { |sq| sq.coords == target }.piece = temp_piece.pop unless temp_piece.empty?
+        end
+      end
+    end
+    true
   end
 end
