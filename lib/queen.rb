1676947ec7694f253b09291f0f26513ca3c58d54
Cypher <raiko4238@gmail.com>
2017-07-14 23:08:21 +0300

    refactor queen.rb

--- lib/queen.rb
+++ lib/queen.rb
@@ -3,19 +3,27 @@ class Queen
   attr_accessor :pos
 
   def initialize(coords)
+    @poss_moves = []
     @pos = coords
     gen_moves
   end
 
-  def gen_moves
-    @poss_moves = []
+  def gen_str_moves
     (-7..7).each do |i|
       @poss_moves << [i,0] << [0,i]
     end
+  end
+
+  def gen_diag_moves
     (1..7).each do |i|
       @poss_moves << [i,i] << [i,-i] << [-i,i] << [-i,-i]
     end
-      @poss_moves.delete_if { |i| i == [0,0] }
+  end
+
+  def gen_moves
+    gen_str_moves
+    gen_diag_moves
+    @poss_moves.delete_if { |i| i == [0,0] }
   end
 end
 
